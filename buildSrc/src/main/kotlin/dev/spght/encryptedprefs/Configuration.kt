6f5e3375312cc7d2749c3210610a1db9604d8c26
Ed Holloway-George <ed@spght.dev>
2025-06-27 16:20:44 +0100

    Bump version to 1.0.3 (#28)

--- buildSrc/src/main/kotlin/dev/spght/encryptedprefs/Configuration.kt
+++ buildSrc/src/main/kotlin/dev/spght/encryptedprefs/Configuration.kt
@@ -7,7 +7,7 @@ object Configuration {
 
     const val majorVersion = 1
     const val minorVersion = 0
-    const val patchVersion = 2
+    const val patchVersion = 3
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
 
     const val versionCode = 100
